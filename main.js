edf91d123c80ffd1a78e7dfc0e5c1bc778c5aaad
Dan Searle <dan.searle@ft.com>
2014-06-18 16:28:47 +0100

    Use o-ft-header v2. Set default techdocs brand colour for header. Update demo config to new format.

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+/*global require*/
 
 require('./src/js/nav');
 require('./src/js/reveals');
 require('./src/js/permalinks');
 require('o-table').wrap('.o-techdocs-content table', 'o-techdocs-table-wrapper');
+
+(function(){
+    "use strict";
+    var Header = require('o-ft-header/main'),
+        headerEl = document.querySelector('[data-o-component="o-ft-header"]');
+    if (headerEl) {
+        new Header(headerEl);
+    }
+}());
