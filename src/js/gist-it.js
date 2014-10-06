6f355c070718ccaa3af76363fe3fb82c7ebf40a2
Andrew Betts <andrew.betts@ft.com>
2014-10-06 19:28:15 +0100

    Linting

--- src/js/gist-it.js
+++ src/js/gist-it.js
@@ -12,5 +12,8 @@ window.oTechdocs.renderGistIt = function(content, file) {
 	// Extract just the prettyprint bit, which can then use techdocs standard styling
 	content = content.replace(/^[\s\S]*(<pre [^>]*>)([\s\S]*?<\/pre>)[\s\S]*$/, "<pre class='prettyprint linenums'>$2");
 
+	/* jshint ignore:start */
+	// Dear future me, yes, you used a document.write here.  Since renderGistit is called during parse and is blocking, we may as well.  We also don't have an element to hook and push the content into.  Since it's just for docs, it's not the end of the world.
 	document.write(content);
+	/* jshint ignore:end */
 };
