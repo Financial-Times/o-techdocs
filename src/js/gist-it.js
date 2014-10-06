997a212bb08291dcaa71d0c95aec3ac4da1802e4
Andrew Betts <andrew.betts@ft.com>
2014-10-06 22:58:39 +0100

    Much improved way of doing gist-it which does not require document.write

--- src/js/gist-it.js
+++ src/js/gist-it.js
@@ -1,19 +1,37 @@
 /**
  * Display Gist-it gists
- *
- * Gist-it is a JSONp endpoint, so we need to attach a handler to the window object.
  */
 'use strict';
 
-if (typeof window.oTechdocs === 'undefined') window.oTechdocs = {};
+var $ = require('jquery');
 
-window.oTechdocs.renderGistIt = function(content, file) {
+$(function() {
 
-	// Extract just the prettyprint bit, which can then use techdocs standard styling
-	content = content.replace(/^[\s\S]*(<pre [^>]*>)([\s\S]*?<\/pre>)[\s\S]*$/, "<pre class='prettyprint linenums'>$2");
+	[].slice.call(document.querySelectorAll('.o-techdocs-gist')).forEach(function(el) {
 
-	/* jshint ignore:start */
-	// Dear future me, yes, you used a document.write here.  Since renderGistit is called during parse and is blocking, we may as well.  We also don't have an element to hook and push the content into.  Since it's just for docs, it's not the end of the world.
-	document.write(content);
-	/* jshint ignore:end */
-};
+		var repo = el.getAttribute('data-repo');
+		var branch = el.getAttribute('data-branch') || 'master';
+		var path = el.getAttribute('data-path');
+		var callbackName = "oTechdocsGistIt"+Math.floor(Math.random()*10000000)+(new Date()).getTime();
+
+		var url = "//gist-it.appspot.com/github/" + repo + "/blob/" + branch + path +"?footer=0&callback=" + callbackName;
+
+		window[callbackName] = function(content) {
+
+			// Extract just the prettyprint bit, which can then use techdocs standard styling
+			content = content.replace(/^[\s\S]*(<pre [^>]*>)([\s\S]*?<\/pre>)[\s\S]*$/, "<pre class='prettyprint linenums'>$2");
+
+			// Replace tabs with 4 spaces since by default tabs in <pre> render at 8-spaces wide
+			content = content.replace(/\t/g, '    ');
+
+			el.innerHTML = content;
+			window[callbackName] = undefined;
+
+			// If available, re-run prettify so that the new content is highlighted
+			if ("prettyPrint" in window) window.prettyPrint();
+		};
+
+		var sc = document.createElement('script'); sc.type = 'text/javascript'; sc.async = true; sc.src = url;
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sc, s);
+	});
+});
