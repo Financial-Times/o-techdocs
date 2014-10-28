01e6225ef040b6aae70e747721564e05a3b41bc2
Alberto Elias <me@alberto-elias.com>
2014-10-28 11:19:39 +0000

    Removed unquotes and linted

--- demos/src/config.js
+++ demos/src/config.js
@@ -1,20 +1,20 @@
 'use strict';
 
 module.exports = {
-    "demos": [
-        {
-            "name": "demo",
-            "template": "main.mustache",
-            "sass": "main.scss",
-            "js": "main.js",
-            "data": {
-                "o-ft-header": require('fs').readFileSync('demos/src/demoheader.html'),
-                "o-techdocs": {
-                    "content": require('fs').readFileSync('demos/src/democontent.html'),
-                    "hasSidebar": true
-                }
-            },
-            "bodyClasses": "o-techdocs o-hoverable-on"
-        }
-    ]
+	"demos": [
+		{
+			"name": "demo",
+			"template": "main.mustache",
+			"sass": "main.scss",
+			"js": "main.js",
+			"data": {
+				"o-ft-header": require('fs').readFileSync('demos/src/demoheader.html'),
+				"o-techdocs": {
+					"content": require('fs').readFileSync('demos/src/democontent.html'),
+					"hasSidebar": true
+				}
+			},
+			"bodyClasses": "o-techdocs o-hoverable-on"
+		}
+	]
 };
