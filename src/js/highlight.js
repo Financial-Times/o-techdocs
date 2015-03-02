e59d43dfb09b52cf20f18aa831173c0d1363d004
Kaelig <kaelig@deloumeau.fr>
2015-03-02 13:05:56 +0000

    Install highlightjs

---
+++ src/js/highlight.js
@@ -0,0 +1,35 @@
+/*global require*/
+/**
+ * Add a second navigation menu to quickly navigate to
+ * anchors in the page.
+ */
+'use strict';
+
+var $ = require('jquery');
+var hljs = require('highlight.js');
+hljs.configure({
+	languages: [
+		'javascript',
+		'xml',
+		'json',
+		'scss',
+		'css',
+		'ruby',
+		'diff',
+		'makefile',
+		'markdown',
+		'php',
+		'python',
+		'java',
+		'sql',
+		'bash',
+		'handlebars',
+		'nginx',
+		'perl',
+		'scala'
+	]
+});
+
+$(function() {
+	hljs.initHighlighting();
+});
