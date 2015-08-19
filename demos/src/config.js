580450e4369fc49b349e289108c68f996f222f87
Kaelig <kaelig@deloumeau.fr>
2015-08-19 12:23:13 +0100

    Update grid dependency

--- demos/src/config.js
+++ demos/src/config.js
@@ -6,7 +6,7 @@ module.exports = {
 		"template": "main.mustache",
 		"sass": "demos/src/scss/main.scss",
 		"js": "main.js",
-		"dependencies": ["o-fonts@^1.4.0", "o-grid@^3.0.0"]
+		"dependencies": ["o-fonts@^1.4.0", "o-grid@^3.2.5"]
 	},
 	"demos": [
 		{
